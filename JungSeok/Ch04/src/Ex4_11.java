b6b6d7c4d74ad98e6ed79c51d2c94e77615b8ae1
lecharl <lecharlhi@gmail.com>
2021-08-24 21:47:16 +0900

    add_array

--- JungSeok/Ch04/src/Ex4_11.java
+++ JungSeok/Ch04/src/Ex4_11.java
@@ -3,12 +3,14 @@ public class Ex4_11 {
 
 	public static void main(String[] args) {
 		// 구구단!
-		for(int i=2; i<10; i++) {
+		int i=2;
+		for(; i<10; i++) {
 			for(int j=1; j<10; j++) {
 				System.out.printf("%d*%d = %d%n", i, j, i*j);
 			}
 			System.out.println();
 		}
+		System.out.println("구구단 "+(i--)+"단까지");
 
 	}
 
