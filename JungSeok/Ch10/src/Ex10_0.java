401e50860927899972e1461d9cdd2d997f591e6e
lecharl <lecharlhi@gmail.com>
2021-09-12 23:50:23 +0900

    ch10start

---
+++ JungSeok/Ch10/src/Ex10_0.java
@@ -0,0 +1,31 @@
+
+public class Ex10_0 {
+
+/*
+ ***날짜와 관련된 필드명
+ YEAR	년도
+ MONTH	월(0부터 시작)	0=1월, ... 6=7월...
+ DATE	일
+ 
+ WEEK_OF_YEAR	그 해 1월1일부터 시작해 지금까지 몇번째 주인지
+ WEEK_OF_MONTH	그 달의 몇번째 주인지
+ 
+ DAY_OF_YEAR	그 해의 몇번째 일인지
+ DAY_OF_MONTH	그 달의 몇번째 일인지
+ 
+ DAY_OF_WEEK	그 주의 요일	1=일요일, ... 7=월요일
+ DAY_OF_WEEK_IN_MONTH	그 달의 몇번째 요일인지
+ 
+ ***시간과 관련된 필드명
+ HOUR			시간 0~11
+ HOUR_OF_DAY	시간 0~23
+ MINUTE			분
+ SECOND			초
+ MILLISECOND	천분의 일초
+ ZONE_OFFSET	GMT기분 시차(천분의 일초 단위) .. 우리나란 +9시간 (9시간 빠르다)
+ AM_PM			오전/오후
+ 
+ */
+
+
+}
