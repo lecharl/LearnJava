d944be57410030e0e290befbab08cb00c1d3d782
lecharl <lecharlhi@gmail.com>
2021-09-19 22:04:28 +0900

    sort..

--- JungSeok/Ch11/src/Ex11_06.java
+++ JungSeok/Ch11/src/Ex11_06.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Ex11_6 {
+public class Ex11_06 {
 
 	public static void main(String[] args) {
 		int[] 	arr = {0, 1, 2, 3, 4};
@@ -32,13 +32,33 @@ public class Ex11_6 {
 		Arrays.setAll(arr7, i -> (int)(Math.random()*6 + 1));//1<= x <7
 		System.out.println("arr7 = "+Arrays.toString(arr7));
 		
-		//***향상된 for문. 우항의 모든 요소를 좌항에 집어넣기
-		for (int x = 0; x < arr7.length; x++) {
-			int i = arr7[x];
-			char[] graph = new char[i];
-			Arrays.fill(graph, '*');
+		//***향상된 for문. 우항(배열)의 모든 요소를 좌항에 집어넣고 본문 시행
+		for(int i : arr7) {
+//		for (int x = 0; x < arr7.length; x++) {
+//			int i = arr7[x];
+			char[] graph = new char[i];	//랜덤값만큼의 길이를 갖는 문자 배열 생성
+			Arrays.fill(graph, '*');	//그 길이만큼 *로 채운다.
 			System.out.println(new String(graph)+i);
+			//***String과 char[]의 변환방법 -> JAVA_48
 		}
+		
+		//비교
+		String[][] str2D = {{"aaa", "bbb"}, {"AAA", "BBB"}};
+		String[][] str2D2 = {{"aaa", "bbb"}, {"AAA", "BBB"}};
+		
+		System.out.println(Arrays.equals(str2D, str2D2));		//1차원
+		System.out.println(Arrays.deepEquals(str2D, str2D2));	//다차원
+		
+		//정렬과 이진검색
+		char[] chArr = {'c','d','a','e','b'};
+		
+		System.out.println("chArr = "+Arrays.toString(chArr));
+		System.out.println("index of 'b' = "+Arrays.binarySearch(chArr, 'b'));
+		System.out.println("== After Sorting ==");
+		Arrays.sort(chArr);//**binarySearch하기 전엔 반드시 정렬먼저!!!
+		System.out.println("chArr = "+Arrays.toString(chArr));
+		System.out.println("index of 'b' = "+Arrays.binarySearch(chArr, 'b'));
+		
 		
 
 	}
